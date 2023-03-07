6a03e35b56eac95732d4948137d6c446a2a134b6
Quas <aaron.hickman201@gmail.com>
2023-03-07 20:30:17 +0000

    Score for finihsed games and date/time for future

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,5 +1,5 @@
-const API_key = "5605e2e711fcfe06959e84c655199e52";
-const leagueId = 39; // Premier League ID
+const API_key = "fafe8cbedaafdf72bc1803ea320e36f7";
+const leagueId = 39;
 const currentYear = 2022;
 const teamsEndpoint = `https://v3.football.api-sports.io/teams?league=${leagueId}&season=${currentYear}`;
 const url = `https://v3.football.api-sports.io/fixtures?league=${leagueId}&season=${currentYear}`;
@@ -22,6 +22,7 @@ async function getTeamId(teamName) {
   const response = await fetch(teamsEndpoint, requestOptions);
   const data = await response.json();
   const team = data.response.find(t => t.team.name.toLowerCase() === teamName.toLowerCase());
+  console.log(data);
   return team.team.id;
 }
 
@@ -30,24 +31,27 @@ async function getFixtures(teamId) {
   const fixturesEndpoint = `https://v3.football.api-sports.io/fixtures?league=${leagueId}&season=${currentYear}&team=${teamId}`;
   const response = await fetch(fixturesEndpoint, requestOptions);
   const data = await response.json();
+  console.log(data);
   return data.response;
 }
 
-// Function to display fixtures
+// Function to display fixtures on HTML page
 function displayFixtures(fixtures) {
-  matchesSection.empty(); 
-  fixtures.forEach(fixture => {
-    const matchTime = fixture.fixture.status.short === "NS" ? fixture.fixture.date : fixture.fixture.status.long;
-    const team1 = fixture.teams.home.name;
-    const team2 = fixture.teams.away.name;
-    const matchContainer = $("<div>").addClass("Match");
-    const team1Card = $("<div>").addClass("Team1").append($("<h2>").text(team1));
-    const versesCard = $("<div>").addClass("Verses").append($("<h2>").text("V"), $("<p>").attr("id", "match-time").text(matchTime));
-    const team2Card = $("<div>").addClass("Team2").append($("<h2>").text(team2));
-    matchContainer.append(team1Card, versesCard, team2Card);
-    matchesSection.append(matchContainer);
-  });
-}
+    matchesSection.empty(); 
+    fixtures.forEach(fixture => {
+      const team1 = fixture.teams.home.name;
+      const team2 = fixture.teams.away.name;
+      const team1Score = fixture.goals.home; 
+      const team2Score = fixture.goals.away; 
+      const status = fixture.fixture.status.short === "FT"? `${team1Score} - ${team2Score}`: fixture.fixture.status.short === "NS"? fixture.fixture.date.substring(0, 16).replace("T", " "): fixture.fixture.status.long;
+      const matchContainer = $("<div>").addClass("Match");
+      const team1Card = $("<div>").addClass("Team1").append($("<h2>").text(team1));
+      const versesCard = $("<div>").addClass("Verses").append($("<h2>").text("V"), $("<p>").attr("id", "match-time").text(status)); 
+      const team2Card = $("<div>").addClass("Team2").append($("<h2>").text(team2));
+      matchContainer.append(team1Card, versesCard, team2Card);
+      matchesSection.append(matchContainer);
+    });
+  }
 
 // Event listener for search button
 searchButton.on("click", async () => {
@@ -62,27 +66,3 @@ searchButton.on("click", async () => {
       }
     }
   });
-
-
-
-
-
-
-        // fetch(url, requestOptions)
-//   .then(res => res.json())
-//   .then(data => {
-//     const teams = new Set(data.response.map(match => match.teams.home.name.toLowerCase()).concat(data.response.map(match => match.teams.away.name.toLowerCase())));
-//     console.log([teams]);
-        
-// //     $(document).ready(function() {   
-// //       $('#searchButton').click(function() {
-// //         const teamName = $('#searchBar input').val()
-// //         if (teams.has(teamName)) {
-// //           alert(`${teamName} is a Premier League team!`);
-// //         } else {
-// //           alert(`${teamName} is not a Premier League team!`);
-// //         }
-// //       });
-// //     });
-// //   })
-// //   .catch(error => console.log('Error:', error));
