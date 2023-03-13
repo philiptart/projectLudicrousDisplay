1c5c3001615bc9dbffafba8649af0baf433d1074
Quas <aaron.hickman201@gmail.com>
2023-03-13 18:45:34 +0000

    grab statistics from API and display for each game

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -5,16 +5,15 @@ const teamsEndpoint = `https://v3.football.api-sports.io/teams?league=${leagueId
 const searchButton = $("#searchButton");
 const searchBar = $("#SearchBar input");
 const matchesSection = $("#Matches");
-const oneMatch = $(".match");
 
 const myHeaders = new Headers();
 myHeaders.append("x-rapidapi-key", API_key);
 myHeaders.append("x-rapidapi-host", "v3.football.api-sports.io");
 
 var requestOptions = {
-	method: 'GET',
-	headers: myHeaders,
-	redirect: 'follow'
+  method: 'GET',
+  headers: myHeaders,
+  redirect: 'follow'
 };
 
 
@@ -35,19 +34,19 @@ async function getFixtures(teamId) {
 }
 
 async function displayFixtures(fixtures) {
-  matchesSection.empty(); 
-  fixtures.forEach(async (fixture) => {
+  matchesSection.empty();
+  for (const fixture of fixtures) {
     const team1 = fixture.teams.home.name;
     const team2 = fixture.teams.away.name;
-    const team1Score = fixture.goals.home; 
-    const team2Score = fixture.goals.away; 
-    const status = fixture.fixture.status.short === "FT"? `${team1Score} - ${team2Score}`: fixture.fixture.status.short === "NS"? fixture.fixture.date.substring(0, 16).replace("T", " "): fixture.fixture.status.long;
+    const team1Score = fixture.goals.home;
+    const team2Score = fixture.goals.away;
+    const status = fixture.fixture.status.short === "FT" ? `${team1Score} - ${team2Score}` : fixture.fixture.status.short === "NS" ? fixture.fixture.date.substring(0, 16).replace("T", " ") : fixture.fixture.status.long;
     const matchContainer = $("<div>").addClass("Match");
     const team1Card = $("<div>").addClass("Team1").append($("<h2>").text(team1));
-    const versesCard = $("<div>").addClass("Verses").append($("<h2>").text("V"), $("<p>").attr("id", "match-time").text(status)); 
+    const versesCard = $("<div>").addClass("Verses").append($("<h2>").text("V"), $("<p>").attr("id", "match-time").text(status));
     const team2Card = $("<div>").addClass("Team2").append($("<h2>").text(team2));
     matchContainer.append(team1Card, versesCard, team2Card);
-    
+
 
     matchesSection.append(matchContainer);
 
@@ -55,40 +54,70 @@ async function displayFixtures(fixtures) {
     matchContainer.click(async (event) => {
       $(".Match").not($(event.currentTarget)).hide();
 
-      const fixtureId = fixture.fixture.id; // Get the fixture ID of the selected match
+      const fixtureId = fixture.fixture.id;
       const statisticsEndpoint = `https://v3.football.api-sports.io/fixtures/statistics?fixture=${fixtureId}`;
       const statisticsResponse = await fetch(statisticsEndpoint, requestOptions);
       const statisticsData = await statisticsResponse.json();
-      console.log(statisticsData); // Print the statistics data to the console
+      console.log(statisticsData);
 
-      const team1Container = $("<div>").addClass("team1-container").append($("<h3>").text(team1));
-      const stats = $("<div>").addClass("statName").append($("<ul class='statList'>").append(
-        $("<li>").text("Posession %"),
-        $("<li>").text("Shots on goal"),
-        $("<li>").text("Fouls"),
-        $("<li>").text("Yellow cards"),
-        $("<li>").text("Red cards"),
-      ));
-      const team2Container = $("<div>").addClass("team2-container").append($("<h3>").text(team2));
-      const teamContainer = $("<div>").addClass("team-container").append(team1Container, stats, team2Container);
+      const team1Stats = statisticsData.response[0].statistics;
+      const team2Stats = statisticsData.response[1].statistics;
+
+      const statNameContainer = $("<div>").addClass("stat-name");
+      const statNameList = $("<ul>").addClass("statNameList");
+
+      statNameList.append(
+        $("<li>").text("possession"),
+        $("<li>").text("shots on goal"),
+        $("<li>").text("fouls"),
+        $("<li>").text("yellow cards"),
+        $("<li>").text("red cards")
+      );
+      statNameContainer.append(statNameList);
+
+      const team1Container = $("<div>").addClass("team1-container");
+      const team1StatsList = $("<ul>").addClass("statList");
+      team1StatsList.append(
+        $("<li>").text(team1Stats[9].value),
+        $("<li>").text(team1Stats[0].value),
+        $("<li>").text(team1Stats[6].value),
+        $("<li>").text(team1Stats[10].value),
+        $("<li>").text(team1Stats[11].value),
+
+
+      );
+      const team1StatsDiv = $("<div>").addClass("team1-stats").append(team1StatsList);
+      team1Container.append(team1StatsDiv);
+
+      const team2Container = $("<div>").addClass("team2-container");
+      const team2StatsList = $("<ul>").addClass("statList");
+      team2StatsList.append(
+        $("<li>").text(team2Stats[9].value),
+        $("<li>").text(team2Stats[0].value),
+        $("<li>").text(team2Stats[6].value),
+        $("<li>").text(team2Stats[10].value),
+        $("<li>").text(team2Stats[11].value)
+      );
+      const team2StatsDiv = $("<div>").addClass("team2-stats").append(team2StatsList);
+      team2Container.append(team2StatsDiv);
+
+      const teamContainer = $("<div>").addClass("team-container").append(team1Container, statNameContainer, team2Container);
       const statisticsContainer = $("<div>").addClass("statistics-container").text("Statistics");
-
-      $(event.currentTarget).after(statisticsContainer,teamContainer);
+      $(event.currentTarget).after(statisticsContainer, teamContainer);
     });
-  });
+  }
 }
-  
 
 // Click event for search button
 searchButton.on("click", async () => {
-    const teamName = searchBar.val().trim();
-    if (teamName !== "") {
-      try {
-        const teamId = await getTeamId(teamName);
-        const fixtures = await getFixtures(teamId);
-        displayFixtures(fixtures);
-      } catch (error) {
-        console.log(error);
-      }
+  const teamName = searchBar.val().trim();
+  if (teamName !== "") {
+    try {
+      const teamId = await getTeamId(teamName);
+      const fixtures = await getFixtures(teamId);
+      displayFixtures(fixtures);
+    } catch (error) {
+      console.log(error);
     }
-  });
+  }
+});
