7258bdcc1cdc0ac2013d11e7ad6d17a00b7a436c
Quas <aaron.hickman201@gmail.com>
2023-03-15 22:48:56 +0000

    Added search history and toggle satistics

--- assets/JS/Footballscript.js
+++ assets/JS/Footballscript.js
@@ -8,6 +8,9 @@ const matchesSection = $("#Matches");
 var item =1
 var colourClass =""
 
+
+  
+
 var itemClassSelector = function(){
   if(item%3==0){
     item++;
@@ -76,22 +79,28 @@ async function displayFixtures(fixtures) {
 
     // Click event to select one match and display statistics
     versesCard.click(async (event) => {
-      // $(".Match").not($(event.currentTarget)).hide();
       var parentEl = event.target.parentElement.parentElement
       $(parentEl).addClass("clicked");
-
+    
       const fixtureId = fixture.fixture.id;
       const statisticsEndpoint = `https://v3.football.api-sports.io/fixtures/statistics?fixture=${fixtureId}`;
       const statisticsResponse = await fetch(statisticsEndpoint, requestOptions);
       const statisticsData = await statisticsResponse.json();
       console.log(statisticsData);
-
+    
       const team1Stats = statisticsData.response[0].statistics;
       const team2Stats = statisticsData.response[1].statistics;
-
+    
+      // Check if the statistics container already exists
+      const existingStatsContainer = $(parentEl).next(".team-container");
+      if (existingStatsContainer.length > 0) {
+        existingStatsContainer.remove(); // Remove the existing container
+        return; // 
+      }
+    
       const statNameContainer = $("<div>").addClass("stat-name FontBold");
       const statNameList = $("<ul>").addClass("statNameList");
-
+    
       statNameList.append(
         $("<li>").text("possession"),
         $("<li>").text("shots on goal"),
@@ -100,7 +109,7 @@ async function displayFixtures(fixtures) {
         $("<li>").text("red cards")
       );
       statNameContainer.append(statNameList);
-
+    
       const team1Container = $("<div>").addClass("team1-container");
       const team1StatsList = $("<ul>").addClass("statList");
       team1StatsList.append(
@@ -109,12 +118,10 @@ async function displayFixtures(fixtures) {
         $("<li>").text(team1Stats[6].value),
         $("<li>").text(team1Stats[10].value),
         $("<li>").text(team1Stats[11].value),
-
-
       );
       const team1StatsDiv = $("<div>").addClass("team1-stats").append(team1StatsList);
       team1Container.append(team1StatsDiv);
-
+    
       const team2Container = $("<div>").addClass("team2-container");
       const team2StatsList = $("<ul>").addClass("statList");
       team2StatsList.append(
@@ -136,7 +143,7 @@ async function displayFixtures(fixtures) {
 // click and keydown for search button
 searchBar.on("keydown", async (event) => {
   if (event.which === 13) { 
-    event.preventDefault(); // prevent the default form submit behavior
+    event.preventDefault();
     const teamName = searchBar.val().trim();
     if (teamName !== "") {
       try {
@@ -151,7 +158,7 @@ searchBar.on("keydown", async (event) => {
 });
 
 searchButton.on("click", async (event) => {
-  event.preventDefault(); // prevent the default form submit behavior
+  event.preventDefault(); 
   const teamName = searchBar.val().trim();
   if (teamName !== "") {
     try {
@@ -162,4 +169,62 @@ searchButton.on("click", async (event) => {
       console.log(error);
     }
   }
+
+
 });
+
+$(document).ready(function() {
+
+  const currentTeams = ["arsenal", "aston villa", "brentford", "brighton", "bournemouth", "chelsea", "crystal palace", "everton", "leeds united", "leicester city", "liverpool", "manchester city", "manchester united", "newcastle", "fulham", "southampton", "tottenham", "nottingham forest", "west ham", "wolves"];
+
+  const previousSearches = JSON.parse(localStorage.getItem("previousSearches")) || [];
+
+  const maxPreviousSearches = 6;
+
+  $("#searchButton").click(search);
+
+  $("#SearchBox").keydown(function(event) {
+    if (event.which === 13) {
+      search();
+    }
+  });
+
+  function search() {
+    const searchTerm = $("#SearchBox").val().toLowerCase();
+    if (currentTeams.includes(searchTerm)) {
+      previousSearches.unshift(searchTerm);
+      if (previousSearches.length > maxPreviousSearches) {
+        previousSearches.pop();
+      }
+      localStorage.setItem("previousSearches", JSON.stringify(previousSearches));
+    }
+    updatePreviousSearchesButtons(previousSearches);
+  }
+
+  updatePreviousSearchesButtons(previousSearches);
+
+  console.log("Previous searches:", previousSearches);
+
+  function updatePreviousSearchesButtons(searches) {
+    $(".previous-searches-container").remove();
+    const uniqueSearches = [];
+    for (var i = 0; i < searches.length; i++) {
+      if (!uniqueSearches.includes(searches[i])) {
+        uniqueSearches.push(searches[i]);
+        if (uniqueSearches.length > 4) {
+          break;
+        }
+      }
+    }
+    for (var i = 0; i < uniqueSearches.length; i++) {
+      const searchTerm = uniqueSearches[i];
+      const button = $("<button>").addClass("button is-outlined previous-searches-button").text(searchTerm);
+      button.click(function() {
+        $("#SearchBox").val($(this).text());
+      });
+      const container = $("<div>").addClass("previous-searches-container").append(button);
+      $("#PrevSearches").append(container);
+    }
+  }
+});
+
